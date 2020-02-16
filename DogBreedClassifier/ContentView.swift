f9a1e225b9bb6e9cd69374d9918da005acbfa5f2
Jerry <wctjerry@gmail.com>
2020-02-16 16:47:12 +0800

    First commit: an MVP with a model to classify dog breed as well as basic layout to show dog image and predicted breed.

--- DogBreedClassifier/ContentView.swift
+++ DogBreedClassifier/ContentView.swift
@@ -7,10 +7,50 @@
 //
 
 import SwiftUI
+import CoreML
+import Vision
 
 struct ContentView: View {
+    @State var label: String = "Classifying"
+    @State var imageName: String = "123"
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Image("\(imageName)")
+             
+            Text("\(label)")
+        }.onAppear(perform: runClassifier)
+    }
+    
+    func runClassifier() {
+        guard let model = try? VNCoreMLModel(for: DogBreedClassifier().model) else {
+            label = "Error converting model..."
+            return
+        }
+        
+        guard let uiImage = UIImage(named: imageName) else {
+            label = "Error converting to UIImage"
+            return
+        }
+        
+        guard let ciImage = CIImage(image: uiImage) else {
+            label = "Error converting to CIIamge"
+            return
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            let results = request.results?.first as? VNClassificationObservation
+            self.label = results?.identifier ?? "Failed to classify"
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([request])
+            } catch {
+                print(error)
+            }
+        }
     }
 }
 
