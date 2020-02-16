8bde8bb1917eaf9c968344e28e4aa2ce960ce57e
Jerry <wctjerry@gmail.com>
2020-02-16 15:02:45 +0800

    Initial Commit

---
+++ DogBreedClassifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DogBreedClassifier
+//
+//  Created by Jerry Wu on 2020/2/16.
+//  Copyright © 2020 Jerry Wu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
