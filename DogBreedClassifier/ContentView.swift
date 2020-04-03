37ac7034c98783f70cbc5cf7804308795aff2dbe
Jerry Wu <wctjerry@gmail.com>
2020-04-03 19:22:30 +0800

    Format the code. No functionality change.

--- DogBreedClassifier/ContentView.swift
+++ DogBreedClassifier/ContentView.swift
@@ -11,31 +11,33 @@ import CoreML
 import Vision
 
 struct ContentView: View {
-    @State var predictedBreed: String = "Classifying"
-    @State var genuineBreed: String = "Unknow"
-    @State private var selectionIndex = 0
-    @State var selectedImage: String = ""
-    @State var selections: [String] = []
-    @State var humanScore: Int = 0
-    @State var botScore: Int = 0
-    @State var alertTitle: String = ""
-    @State var alertMessage: String = ""
-    @State var showingAlert: Bool = false
+    
+    // Pridicted and genuine breed
+    @State private var predictedBreed: String = "Classifying"
+    @State private var genuineBreed: String = "Unknow"
+    
+    // Shown images
+    @State private var selectedImage: String = ""
+    @State private var selections: [String] = []
+    
+    // Alerting and progress
+    @State private var alertTitle: String = ""
+    @State private var alertMessage: String = ""
+    @State private var showingAlert: Bool = false
+    @State private var humanScore: Int = 0
+    @State private var botScore: Int = 0
     
     var gameData: GameData
     
-    func randomImage () -> String {
-        // Select a random image to show
-        
-        return self.gameData.images.randomElement() ?? "Fail to randomize"
-    }
     
     func initGame() {
-        //         Initiate game setting, including:
-        //         1. Randomize and show a dog image
-        //         2. Predict and store the dog breed in the image
-        //         3. Find out and store the dog's genuine breed
-        //           Note this will modify ContentView's property
+        //  Initiate game setting, including:
+        //    1. Randomize and show a dog image on the screen
+        //    2. Predict the dog breed by classifier model
+        //    3. Find out and store the dog's genuine breed
+        //    4. Generate four selections to be selected by the player,
+        //       including predicted and genuine breed
+        
         self.selectedImage = self.randomImage()
         self.predictedBreed = self.runClassifier(image: self.selectedImage)
         print("Predicted breed: \(self.predictedBreed)")
@@ -50,10 +52,18 @@ struct ContentView: View {
         
     }
     
+    
+    func randomImage () -> String {
+        // Select a random image to show on the screen
+        
+        return self.gameData.images.randomElement() ?? "Fail to randomize"
+    }
+    
+    
     func randomBreedSelections (selections: [String]) -> [String] {
         // Generate up to four random and distinct choices of dog breed
         
-        var selections = Array(Set(selections)) // Remove duplicates
+        var selections = Array(Set(selections)) // Remove duplicates, if the predicted and genuine breed are the same
         
         while selections.count < 4 {
             var randomBreed = self.gameData.dataSet[self.randomImage()]!
@@ -70,95 +80,6 @@ struct ContentView: View {
         return selections
     }
     
-    func calculateRoundResult(selection: String) {
-        // Identify win or lose in a round
-        
-        if selection == self.genuineBreed { // Human is correct
-            self.humanScore += 1
-            self.alertTitle = "You are correct!"
-        } else {
-            self.alertTitle = "You are wrong!"
-        }
-        
-        if self.predictedBreed == self.genuineBreed { // Bot is correct
-            self.botScore += 1
-        }
-        
-        self.alertMessage = "You've won \(self.humanScore) \(self.humanScore == 0 ? "point" : "points")"
-        
-        self.alertMessage += ", whereas AI has won \(self.botScore) \(self.botScore == 0 ? "point" : "points")"
-        
-        self.showingAlert = true
-    }
-    
-    struct styleSelection: View {
-        // Style four selections
-        var with: String
-        
-        var body: some View {
-            Text(with)
-                .font(.title)
-                .padding()
-                .frame(minWidth: 300, maxWidth: .infinity)
-                .background(Color.white)
-                .cornerRadius(40)
-                .foregroundColor(.black)
-                .overlay(RoundedRectangle(cornerRadius: 40)
-                    .stroke(Color.blue, lineWidth: 5))
-        }
-        
-        
-    }
-    
-    var body: some View {
-        
-        ZStack {
-            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
-                .edgesIgnoringSafeArea(.all)
-            
-            
-            VStack {
-                Image(selectedImage)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 300, height: 300, alignment: .center)
-                    .fixedSize()
-                
-                ForEach(self.selections, id: \.self) { selection in
-                    Button(action: {
-                        self.calculateRoundResult(selection: selection)
-                    }) {
-                        styleSelection(with: selection)
-                    }
-                    .padding(8)
-                }
-                
-                Button(action: {
-                    self.initGame()
-                }) {
-                    HStack{
-                        Image(systemName: "arrow.clockwise")
-                            .font(.title)
-                        
-                        Text("Try another image")
-                            .font(.title)
-                    }
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(Color.red)
-                    .cornerRadius(40)
-                    
-                    
-                }
-                
-            }
-        }.onAppear(){
-            self.initGame()
-        }
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text(self.alertTitle), message: Text(self.alertMessage), dismissButton: .default(Text("Continue")))
-        }
-    }
     
     func runClassifier(image: String) -> String {
         // Return a predicted breed of the shown image dog
@@ -193,7 +114,94 @@ struct ContentView: View {
         }
         return predictedBreed
     }
+    
+    
+    func calculateRoundResult(selection: String) {
+        // Identify win or lose in a round
+        
+        if selection == self.genuineBreed { // Human is correct
+            self.humanScore += 1
+            self.alertTitle = "You are correct!"
+        } else {
+            self.alertTitle = "You are wrong!"
+        }
+        
+        if self.predictedBreed == self.genuineBreed { // Bot is correct
+            self.botScore += 1
+        }
+        
+        self.alertMessage = "You've won \(self.humanScore) \(self.humanScore == 0 ? "point" : "points")"
+        
+        self.alertMessage += ", whereas AI has won \(self.botScore) \(self.botScore == 0 ? "point" : "points")"
+        
+        self.showingAlert = true
+    }
+    
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image(selectedImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300, alignment: .center)
+                    .fixedSize()
+                
+                ForEach(self.selections, id: \.self) { selection in
+                    Button(action: {
+                        self.calculateRoundResult(selection: selection)
+                    }) {
+                        styleSelection(with: selection)
+                    }
+                    .padding(8)
+                }
+                
+                Button(action: {
+                    self.initGame()
+                }) {
+                    HStack{
+                        Image(systemName: "arrow.clockwise")
+                            .font(.title)
+                        
+                        Text("Try another image")
+                            .font(.title)
+                    }
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(40)
+                }
+            }
+        }.onAppear(){
+            self.initGame()
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text(self.alertTitle), message: Text(self.alertMessage), dismissButton: .default(Text("Continue")))
+        }
+    }
+    
+    struct styleSelection: View {
+         // Style for four selections
+         var with: String
+         
+         var body: some View {
+             Text(with)
+                 .font(.title)
+                 .padding()
+                 .frame(minWidth: 300, maxWidth: .infinity)
+                 .background(Color.white)
+                 .cornerRadius(40)
+                 .foregroundColor(.black)
+                 .overlay(RoundedRectangle(cornerRadius: 40)
+                     .stroke(Color.blue, lineWidth: 5))
+         }
+     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
